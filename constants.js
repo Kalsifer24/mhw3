f1333570e9ea6767a9c4bd1843cce21af540c5a7
Kalsifer24 <102297248+Kalsifer24@users.noreply.github.com>
2022-04-19 14:27:09 +0200

    Add files via upload

--- constants.js
+++ constants.js
@@ -1,45 +1,45 @@
 ﻿const RESULTS_MAP = {
-'blep': {
+'0': {
 title: 'Bad Guy',
 contents: "Tifi quasi sempre per l'antagonista, non ami le storie a lieto fine e speri che tutto finisca sempre con la distruzione dell'universo.",
 image: "images/R_1.jpg",
 },
-'burger': {
+'1': {
 title: 'SIMP',
 contents: "Guardi solo anime romantici con storie d'amore a lieto fine, i tuoi standard di bellezza sono stati totalmente sovrascritti da quelli dei personaggi 2D.",
 image: "images/R_2.jpg",
 },
-'cart': {
+'2': {
 title: 'Boomer',
 contents: "Guardi solo anime usciti prima del 1999 e ti lamenti costantemente di come i nuovi anime in computer grafica e la risoluzione in 16:9 sia sopravvalutata e priva di contenuto.",
 image: "images/R_3.jpg",
 },
-'dopey': {
+'3': {
 title: 'Ghibli fan',
 contents: "Nella tua vita hai visto solo film dello studio ghibli ma ti rapporti con gli altri come se fossi il più grande intenditore di anime in città.",
 image: "images/R_4.jpg",
 },
-'happy': {
+'4': {
 title: 'Sentimentale',
 contents: "Simpatizzi per i personaggi apparentemente più deboli e vulnerabili, cominci a piangere non appena parte la musica drammatica e pensi che il tuo personaggio preferito stia per morire.",
 image: "images/R_5.jpg",
 },
-'nerd': {
+'5': {
 title: 'Nostalgico',
 contents: "Guardi un pò tutto ma niente ti entusiasma, ogni opera che finisci ti sembra mediocre e pensi che niente sarà mai meglio di Dragon Ball.",
 image: "images/R_6.jpg",
 },
-'shy': {
+'6': {
 title: 'Plot-twister',
 contents: "Odi le trame lineari e banali, vuoi sempre rimanere sorpreso dagli eventi narrati ma ti lamenti se il finale non è quello che ti sei creato nella tua testa dopo i primi 5 minuti del primoi episodio.",
 image: "images/R_7.jpg"
 },
-'sleeping': {
+'7': {
 title: 'Psicologo',
 contents: "La maggior parte dei dialoghi che ascolti negli anime sono i pensieri interminabili dei personaggi che spiegano come siano riusciti a predirre ogni mossa del proprio avversario avendolo visto di sfuggita per sbaglio per un secondo mentre passeggiavano per strada .",
 image: "images/R_8.jpg"
 },
-'sleepy': {
+'8': {
 title: 'Otaku',
 contents: "Guardi solo anime di massa e a stento ricordi il nome del protagonista, sparli di ogni opera che non abbia almeno 8/10 su Myanimelist e sei odiato da tutti.",
 image: "images/R_9.jpg"
--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 //Funzioni 
+function Start(){
+
+    quiz.classList.remove('hidden');
+
+    for (let button of buttons)
+    {
+    button.classList.add('hidden');
+    }
+
+    fetch('https://nekos.best/api/v2/neko?amount=20').then(onResponse).then(onJson);
+}
 
 function selected(event)
     {
@@ -14,13 +25,17 @@ function selected(event)
     image.classList.replace("unselected" , "selected");
     ckbox.src = 'images/checked.png';
     }
-    
+
+   
     Controlli(image.dataset.questionId,image.dataset.choiceId);
     Risposte(image.dataset.questionId,image.dataset.choiceId);
 
-    }
+}
 
 function Controlli(domanda,id){
+
+const images = document.querySelectorAll('.choice-grid div');
+
   for(const box of images)
   {
       if(box.classList !="selected" && domanda==box.dataset.questionId && id!=box.dataset.choiceId){
@@ -38,20 +53,27 @@ function Controlli(domanda,id){
 }
  
 function Risposte(domanda,id){
+
+const images = document.querySelectorAll('.choice-grid div');
+
   for(const box of images){
         if(box.classList == "selected" && domanda=="one")
         {
             answers.Ans1=box.dataset.choiceId;
+           
+            console.log(answers);
         }
 
         if(box.classList == "selected" && domanda=="two")
         {
             answers.Ans2=box.dataset.choiceId;
+            console.log(answers);
         }
 
         if(box.classList == "selected" && domanda=="three")
         {
             answers.Ans3=box.dataset.choiceId;
+            console.log(answers);
         }
     }
         
@@ -81,56 +103,58 @@ function Risposte(domanda,id){
 }
 
 function Reset(){
+    
+const images = document.querySelectorAll('.choice-grid div');
 
-    for (let box of images){
-    box.addEventListener('click', selected);
-    const checkbox = box.querySelector('.checkbox');     
+    for (let image of images){
+    image.addEventListener('click', selected);
+    const checkbox = image.querySelector('.checkbox');     
     checkbox.src = 'images/unchecked.png';
-    box.className = "";
+    image.className = "";
     }
+
+    for (let grid of grids)
+    {
+        grid.innerHTML = '';
+        quiz.classList.add('hidden');
+    }
+
+    for (let button of buttons)
+    {
+    button.classList.remove('hidden');
+    }
+
 
     answers.Ans1 = "";
     answers.Ans2 = "";
     answers.Ans3 = "";
 
-    
     result.classList.add("hidden");
 
 }
 
-function Start(){
-    let quiz = document.querySelector(".quiz")
-    quiz.classList.remove('hidden');
-    for (let button of buttons)
-    {
-    button.classList.add('hidden');
-    }
-
-    
-    fetch('https://nekos.best/api/v2/neko?amount=20').then(onResponse).then(onJson);
-    
-
-}
 
 function onResponse(response){
-    console.log("siamo nella response");
+    
     return response.json();
 }
 
-
 function onJson(json){
-    console.log('json ricevuto');
-    let j = 0;
     
+    let j = 0;
+    let list = Array.prototype.slice.call(grids); 
+    
+
     for(let grid of grids){
-       
-        for( let i = 0; i<9 ; i++){
+
+        for( let i = 0; i<6 ; i++){
             
             let images = json.results[j];
             j++
             if(j>=20){
                 j=0;
             }
+
             const block = document.createElement('div');
             const image = document.createElement('img');
             const chkbox = document.createElement('img');
@@ -138,26 +162,49 @@ function onJson(json){
             image.src = images.url;
             chkbox.src = "images/unchecked.png";
             chkbox.classList.add("checkbox");
+           
+            
+            if(list.indexOf(grid) == "0"){
+            block.setAttribute('data-question-id',"one");
+            }
+            else if(list.indexOf(grid) == "1"){
+            block.setAttribute('data-question-id',"two");
+            }
+            else if(list.indexOf(grid) == "2"){
+            block.setAttribute('data-question-id',"three");
+            }
+             
+            block.setAttribute('data-choice-id', i);
 
             grid.appendChild(block);
             block.appendChild(image);
             block.appendChild(chkbox);
         }    
     }  
-   
+
+    const images = document.querySelectorAll('.choice-grid div');
+    
+    for (let image of images)
+    {
+    image.addEventListener('click', selected);
+    }
+
 }
 
 
 
 //Selettori
+
+let quiz = document.querySelector(".quiz")
 const grids = document.querySelectorAll(".choice-grid")
-const buttons = document.querySelectorAll("div button");
+const buttons = document.querySelectorAll("#category button");
 const reset = document.querySelector("footer button");
 const res_img = document.querySelector("footer img");
 const titolo = document.querySelector("footer h1");
 const descrizione = document.querySelector("footer p");
 const result = document.querySelector("footer div");
 const images = document.querySelectorAll('.choice-grid div');
+
 
 let answers = {
     Ans1:"",
@@ -166,11 +213,6 @@ let answers = {
 };
 
 //EventListeners
-
-for (let image of images)
-{
-image.addEventListener('click', selected);
-}
 
 for (let button of buttons)
 {
